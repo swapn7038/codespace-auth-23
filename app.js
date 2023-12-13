6dfca97f2c654245ffefd198dd53e36b67a1bc77
Swapnil Shelke <82700394+swapn7038@users.noreply.github.com>
2023-12-13 08:59:47 +0000

    register route created

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+require('dotenv').config()  
+require("./database/database.js").connect();        
 const express = require("express")
-require('dotenv').config()
-// const { connect } = require("mongoose")
-
-require("./database/database.js").connect();
+const Usre = require("./models/user.js");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const app = express()
 app.use(express.json())
@@ -16,15 +17,50 @@ app.get('/', (req, res) => {
 app.post("/register", async (req, res) => {
     try {
         // get data from body or url
+        const {firstName, lastName, email, password} = req.body
         // all data should exists 
-        // check if user allready exists
+        if(!(firstName && lastName && email && password)){
+            res.status(400).send("All fields are compulsary...")
+        }
+
+        // check if user allready exists - email
+        const existingUser = await User.findOne({ email })
+
+        if(existingUser) {
+            res.status(401).send("User allready existed...")
+        }
+
         // encrypt the password
+        const enCryptedPassword = await bcrypt.hash(password, 10)
+
         // save user in DB
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            password: enCryptedPassword
+
+        })
+
+
         // generate token for user and send it
+        const token = jwt.sign(
+            {id: user._id, email}, "shhhh" ,
+            // provide here something like process.env.jwtsecret
+            {
+                expiresIn: "2h"
+            })
+
+            user.token = token
+            user.password = undefined
+
+            res.status(201).json(user)
     } catch (error) {
         console.log(error);
     }
 })
+
+
 
 
 app.listen(PORT, () => {
