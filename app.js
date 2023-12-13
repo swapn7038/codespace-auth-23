0e537bfa1d3570db1c3fb829ca7fe87174604503
Swapnil Shelke <82700394+swapn7038@users.noreply.github.com>
2023-12-13 09:36:58 +0000

    login route added, testing is remaining

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 require('dotenv').config()  
 require("./database/database.js").connect();        
 const express = require("express")
-const Usre = require("./models/user.js");
+const User = require("./models/user.js");
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 const PORT = 4000
 
@@ -60,7 +62,54 @@ app.post("/register", async (req, res) => {
     }
 })
 
+app.post('.login', async(req, res) => {
+    try {
+      // get all data from frontend or user
+      const {email, password} = req.body
+  
+      // add validation
+      if(!(email && password)){
+          res.status(400).send("Send all the data please...");
+      }
+      // find user in DB
+      const user = await User.findOne({email})
+  
+      // Assignment - What if there is not user
+      if(!(user)){
+          console.log('Register... This is not a registered email');
+          process.exit(1);
+      }
+      // match the password
+      if(user && (await bcrypt.compare(password, user.password))){
+          const token = jwt.sign(
+            {id:user._id},
+            "shhhh",
+            {
+                expiresIn:"2h"
+            }
+          );
 
+          user.token = token
+          user.password = password
+      }
+
+      // cookie section
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly : true
+    }
+    res.status(200).cookie("token", token, options).json({
+        success: true,
+        token,
+        user
+    })
+
+    
+      // send a token
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 
 app.listen(PORT, () => {
